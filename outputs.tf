dfe5a47f03c1dfbefa980c8492d7781ba834eeac
duda30 <dudalombardi30@gmail.com>
2022-10-17 14:02:49 -0300

    criação de outputs dos módulos filhos

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,31 @@
 output "instance_id" {
-  value = modules.computing-module.aws_instance.ec2-maria.id
+  value = module.computing-module.instance_id
 }
 
 output "subnet_id" {
-  value = modules.network-module.aws_subnet.subnet-publica.id
+  value = module.network-module.subnet_id
 }
 
 output "igw_id" {
-  value = modules.network-module.aws_internet_gateway.igw-maria.id
+  value = module.network-module.igw_id
 }
 
 output "security_group_id" {
-  value = modules.computing-module.aws_security_group.allow_22_80_myip.id
+  value = module.computing-module.security_group_id
 }
 
 output "private_ip" {
-  value = modules.computing-module.aws_instance.ec2-maria.private_ip
+  value = module.computing-module.private_ip
 }
 
+output "bucket_name" {
+  value = module.back-end-s3.bucket_name
+}
+
+output "bucket_acl" {
+  value = module.back-end-s3.bucket_acl
+}
+
+output "bucket_versioning" {
+  value = module.back-end-s3.bucket_versioning
+}
