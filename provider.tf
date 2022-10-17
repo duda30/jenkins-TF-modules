84dd84a27325a38c250508f95137f8953919b57a
duda30 <dudalombardi30@gmail.com>
2022-10-17 14:03:44 -0300

    config. provider

--- provider.tf
+++ provider.tf
@@ -1,8 +1,14 @@
+
 terraform {
   required_providers {
     aws = {
       version = "4.34.0"
     }
+  }
+  backend "s3" {
+    bucket = "my-bucket-mary-maria12312"
+    key    = "desafio-jenkins/usando-modulos/terraform.tfstate"
+    region = "us-east-2"
   }
 }
 
