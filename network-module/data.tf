842fc59df359669afa9cd5645735d3d85fb5f3d3
duda30 <dudalombardi30@gmail.com>
2022-10-17 13:59:22 -0300

    revisão - rede-tf

---
+++ network-module/data.tf
@@ -0,0 +1,3 @@
+data "aws_vpc" "id-vpc-data" {
+  id = var.vpc_id
+}
---
+++ network-module/outputs.tf
@@ -0,0 +1,8 @@
+output "subnet_id" {
+  value = aws_subnet.subnet-publica.id
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.igw-maria.id
+}
+
